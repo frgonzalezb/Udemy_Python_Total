8170ab168827aa60b65f7facafa2d912d9ddb888
Francisco <fr.gonzalezb@duocuc.cl>
2024-02-13 13:21:05 -0300

    Update asistencia_personal.py

--- Dia_14/asistencia_personal.py
+++ Dia_14/asistencia_personal.py
@@ -10,6 +10,8 @@ comparación de las tomas con imágenes almacenadas en una base de datos.
 import os
 import cv2
 import face_recognition as fr
+import numpy as np
+
 from pathlib import Path
 
 
@@ -49,4 +51,68 @@ def codificar(imagenes):
 
 
 lista_empleados_codificada = codificar(mis_imagenes)
-print(lista_empleados_codificada)
+
+# Tomar imagen de webcam
+captura = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+# Leer la imagen de la camara
+exito, imagen = captura.read()
+
+if not exito:
+    print('No se ha podido tomar la captura.')
+    exit()
+
+# Reconocer cara en captura
+cara_captura = fr.face_locations(imagen)
+
+# Codificar cara capturada
+cara_captura_codificada = fr.face_encodings(imagen, cara_captura)
+
+# Buscar coincidencias
+for caracodif, caraubic in zip(cara_captura_codificada, cara_captura):
+    coincidencia = fr.compare_faces(lista_empleados_codificada, caracodif)
+    distancias = fr.face_distance(lista_empleados_codificada, caracodif)
+    print(distancias) # dbg
+
+    indice_coincidencia = np.argmin(distancias)
+
+    # Mostrar coincidencias
+    if distancias[indice_coincidencia] > 0.6:
+        print('Su rostro no coincide con ninguno de nuestros empleados.')
+        exit()
+    
+    print('Bienvenido a su nueva jornada.') # dbg
+
+    # Buscar el nombre del emplado encontrado
+    nombre = nombres_empleados[indice_coincidencia]
+
+    y1, x2, y2, x1 = caraubic
+    cv2.rectangle(
+        imagen, 
+        (x1, y1), 
+        (x2, y2), 
+        (0, 255, 0), 
+        2
+    )
+    cv2.rectangle(
+        imagen, 
+        (x1, y2 - 35), 
+        (x2, y2), 
+        (0, 255, 0), 
+        cv2.FILLED
+    )
+    cv2.putText(
+        imagen, 
+        nombre, 
+        (x1 + 6, y2 - 6), 
+        cv2.FONT_HERSHEY_COMPLEX, 
+        1, 
+        (255, 255, 255), 
+        2
+    )
+
+    # Mostrar imagen obtenida
+    cv2.imshow('Imagen web', imagen)
+
+    # Mantener ventana abierta
+    cv2.waitKey(0)
